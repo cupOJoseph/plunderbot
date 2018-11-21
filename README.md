ff748d9b91a5e16ba1c8d39543cab163d11234b0
jschiarizzi <jschiarizzi@gwu.edu>
2018-11-21 21:08:13 +0000

    add dependency and argument tracking

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # plunderbot
 An bot that finds arbitrage opportunities on OpenSea and plunders them for profit. Uses OpenSea.js
+
+## Installation
+```
+npm install plunderbot
+```
+
+## Dependencies
+- Node
+- yargs 12.0.5
--- bin/plunder.js
+++ bin/plunder.js
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
 
-console.log("Plunder!!");
+//console.log("Plunder!!");
+
+var argv = require('yargs')
+    .usage('Usage: $0 -wallet_mnemonic [string of words] -wallet_address [ethereum address] -eth_budget [num] -eth_price_threshold [num] increase_percentage [number 0 - 100] -asset_classes [cryptokitties,axie]')
+    .demandOption(['wallet_mnemonic','wallet_address', 'eth_budget', 'eth_price_threshold', 'increase_percentage', 'asset_classes'])
+    .argv;
+
+/* Search for cheap Kitties */
+
+
+/* Buy Cheap Kitties */
+ 
+
+/* Sell kitties for profit on Open Sea using OpenSea.js */
