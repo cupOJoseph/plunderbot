b05e3a3d4b5498688257160687bb42f50e4763e7
jschiarizzi <jschiarizzi@gwu.edu>
2018-11-21 22:10:53 +0000

    handle mnemonic in quotes

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ An bot that finds arbitrage opportunities on OpenSea and plunders them for profi
 npm install plunderbot
 ```
 
+## Usage
+Use `plunderbot` in your terminal or bash window. Try `plunderbot --help` for options.
+
+### Arguments
+- wallet_mnemonic: The mnemonic for the wallet to use. *Enter seed phrase words surrounded by quotes* Example: `$plunderbot -wallet_mnemonic "witch collapse practice feed shame open despair creek road again ice least" ...`
+- wallet_address: The address of the wallet to use.
+- eth_budget: The amount of ETH the wallet is allowed to spend.
+- price_increase_percentage: How much higher to relist the items.
+- asset_classes: Comma-separated list of asset classes (e.g., cryptokitties,mlbcrypto)
+
+
 ## Dependencies
 - Node
 - yargs 12.0.5
--- bin/plunder.js
+++ bin/plunder.js
@@ -3,9 +3,22 @@
 //console.log("Plunder!!");
 
 var argv = require('yargs')
-    .usage('Usage: $0 -wallet_mnemonic [string of words] -wallet_address [ethereum address] -eth_budget [num] -eth_price_threshold [num] increase_percentage [number 0 - 100] -asset_classes [cryptokitties,axie]')
+    .usage('Usage: $0 -wallet_mnemonic [string of words with underscores] -wallet_address [ethereum address] -eth_budget [num] -eth_price_threshold [num] increase_percentage [number 0 - 100] -asset_classes [cryptokitties,axie]')
     .demandOption(['wallet_mnemonic','wallet_address', 'eth_budget', 'eth_price_threshold', 'increase_percentage', 'asset_classes'])
     .argv;
+    
+    /* Example
+    plunderbot --wallet_mnemonic "apple biscuit church" --wallet_address 0x123abc --eth_budget 0.5 --eth_price_threshold 0.01 --increase_percentage 5 --asset_classes cryptokitties,axies
+
+    */
+    
+
+
+/* Sanity Checks */
+//make sure you have that much eth in the account TODO
+
+// make sure the price threshold is smaller than budget and is a small-ish number TODO
+// if its not a good small number give a warning and suggestion TODO
 
 /* Search for cheap Kitties */
 
