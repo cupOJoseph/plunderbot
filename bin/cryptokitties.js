b25e3153be414202ca2f5ed78f0f3230efeebb47
jschiarizzi <jschiarizzi@gwu.edu>
2018-11-22 01:03:42 +0000

    routing init. dependencies for trades

--- bin/cryptokitties.js
+++ bin/cryptokitties.js
@@ -1,6 +1,11 @@
 function CryptoKittyArbitrage (argv) {
-    // 
-    console.log("$$$");
+    // Go find cheap kittes less than argv.eth_price_threshold TODO
+    //console.log("finding kitties under " + argv.eth_price_threshold);
+    
+    //Buy cheap kitties directly from kitty contract
+    
+    //list cheap kitties on opensea
+    
 }
 
 module.exports.CryptoKittyArbitrage = CryptoKittyArbitrage;
