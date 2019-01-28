2f1dc8c5159e219e63cf6ae5aba65bbd142536ba
morph <jschiarizzi@gwu.edu>
2019-01-28 15:30:44 -0800

    move getKitty to helper scripts

--- bin/helpers.js
+++ bin/helpers.js
@@ -6,6 +6,8 @@
 * bin scripts for easier readability.
 */
 var bn = require("bignumber.js");
+var hh = require("http-https");
+
 
 
 /* Get the user's ETH balance from web3.
@@ -25,6 +27,38 @@ function checkETHBalance (web3){
       });
     }
   });
+}
+
+function getCheapKitty(){
+  //note limit in the url is set to 1. this should be a variable, could be interesting TODO
+  var cheap_kitty_url = "https://api.cryptokitties.co/v2/kitties?offset=0&limit=1&include=sale&parents=false&authenticated=false&orderBy=current_price&orderDirection=asc&total=true";
+  // Identify cheap kitties
+  var req = hh.get(cheap_kitty_url, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log("Arrrr, matey, we found somethin'");
+    //Cheapest kitty is:
+    var kitties = JSON.parse(data).kitties;
+    var kitty = kitties[0];
+    //console.log(kitty);
+
+    var wei_price = kitty.auction.current_price;
+
+    console.log("price in wei: ", wei_price);
+  });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+  return kitty;
 }
 
 
